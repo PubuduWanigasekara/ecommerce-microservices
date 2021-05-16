d7b7a932a94f410e836058cec3f412ad6f0052fe
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-05-16 22:41:14 +0530

    added api gatwaty

---
+++ api-gateway/src/test/java/com/mtit/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtit.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
