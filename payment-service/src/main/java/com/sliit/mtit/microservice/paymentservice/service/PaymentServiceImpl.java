1de8de919abedf43c5d426e783857d4a4ed29729
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-05-18 15:43:57 +0530

    remove

--- payment-service/src/main/java/com/sliit/mtit/microservice/paymentservice/service/PaymentServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.sliit.mtit.microservice.paymentservice.service;
-
-public class PaymentServiceImpl {
-
-}
--- product-service/src/main/java/com/mtit/productservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mtit.productservice.repository;
-
-import com.mtit.productservice.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-
-    Product findByProductId (Long productId);
-}
