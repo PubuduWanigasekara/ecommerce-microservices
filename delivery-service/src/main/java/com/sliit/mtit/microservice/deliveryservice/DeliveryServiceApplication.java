efb3b73803afbbc1e6f7d3548b7845b6b830a5ab
Pubudu Arosha <it18108514@my.sliit.lk>
2021-05-18 22:44:22 +0530

    delivery service added

---
+++ delivery-service/src/main/java/com/sliit/mtit/microservice/deliveryservice/DeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.mtit.microservice.deliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryServiceApplication.class, args);
+	}
+
+}
---
+++ delivery-service/src/test/java/com/sliit/mtit/microservice/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.mtit.microservice.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
