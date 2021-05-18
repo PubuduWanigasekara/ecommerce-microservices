fd832aa223ab31941162829d0e7fb98f6f712d0f
Pubudu Arosha <it18108514@my.sliit.lk>
2021-05-18 22:41:55 +0530

    location service added

---
+++ location-service/src/main/java/com/sliit/mtit/microservice/locationservice/LocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.mtit.microservice.locationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationServiceApplication.class, args);
+	}
+
+}
